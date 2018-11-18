ee4be8812fa77a29a5afe94e9626f2c44eb38d3e
Josh Varney <joshcainvarney@gmail.com>
2018-11-17 20:55:15 -0500

    working on figuring out the game logic

--- life_game.rb
+++ life_game.rb
@@ -1,43 +1,61 @@
-def alive_cell(index_hash) 
-  index_hash.each do |key, value|
-    if value == true
-      return value
-    else
-      return value
-    end    
-  end
-end
 
-class Draw_grid
-  def initialize()
-    grid()
-    make_grid()
+def game_array(size)
+  array = Array.new(size * size)
+  counter = 0
+  array.each do
+    array[counter] = true
+    counter += 1
   end  
-  def grid()
-    index_array = []
-    array = Array.new(9).map.with_index{ |x, i| i }
-    array.each do |element|
-        index_array << element
-        index_array << true
-    end
-    @index_hash = Hash[*index_array]      
-    return @index_hash
-  end 
-  def make_grid()
-    @the_grid = ""
-    @index_hash.keys.each do |a|
-      @the_grid << index
-      case index % 3                                     
-      when 0, 1 then @the_grid << "|"
-      when 2 then @the_grid << "\n-----------\n" unless index == 8
-      end
-    end
-    return @the_grid  
-  end
-  attr_reader :the_grid
-  attr_reader :index_hash
+  return array
 end
-this = Draw_grid.new
-puts this.index_hash.keys
-p this.the_grid      
+def find_neighbors(index, array)
+  neighbor_array = []
+  right_side = index + 1
+  size = Math.sqrt(array.count).to_i
+  end_row_first = array.count - size          
+  unless right_side % size == 0
+    neighbor1 = index + 1
+    neighbor_array << neighbor1
+  end  
+  unless index % size == 0
+    neighbor2 = index - 1
+    neighbor_array << neighbor2
+  end
+  unless (end_row_first..array.count).include?(index)
+    neighbor3 = size + index
+    neighbor_array << neighbor3
+  end    
+  unless right_side % size == 0 || (end_row_first..array.count).include?(index)
+    neighbor4 = size + index + 1
+    neighbor_array << neighbor4
+  end  
+  unless index % size == 0 || (end_row_first..array.count).include?(index)
+    neighbor5 = size + index - 1
+    neighbor_array << neighbor5
+  end 
+  unless index - size < 0
+    neighbor6 = index - size 
+    neighbor_array << neighbor6
+  end
+  unless index - size < 0 || right_side % size == 0 
+    neighbor7 = index - size + 1
+    neighbor_array << neighbor7
+  end  
+  unless index % size == 0 || index - size < 0
+    neighbor8 = index - size - 1
+    neighbor_array << neighbor8
+  end  
+  return neighbor_array
+end
+def find_new_state(state, index, size)
+  array = game_array(3)
+  neighbors = find_neighbors(index, array)
+  neighbors = [false]
+  if neighbors.include?(true) == false 
+    state = false
+  end
+  return state   
+end
+# class Start_game
+#   def initialize
 
--- life_game_test.rb
+++ life_game_test.rb
@@ -3,10 +3,52 @@ require_relative "life_game.rb"
 
 class Test_Life_Game <Minitest::Test
 
-  def test_that_a_cell_is_alive
-    index_hash = {
-      1 => true
-    }
-    assert_equal(true, alive_cell(index_hash))
+  def test_alive_cell_dies_when_no_alive_neighbors
+    alive = true
+    dead = false
+    new_state = find_new_state(alive, 0)
+    assert_equal(dead, new_state)
   end
+  def test_that_array_right_size
+    size = 3
+    assert_equal(9, game_array(size).length)
+  end  
+  def test_that_array_starts_out_everything_true
+    size = 3
+    assert_equal([true], game_array(size).uniq)
+  end
+  # def test_that_can_find_neighbors
+  #   assert_equal([], find_neighbors(0, game_array(3)))
+  # end 
+  # def test_that_modulo_is_working_so_far
+  #   assert_equal([0, 3], find_neighbors(1, game_array(3)))
+  # end
+  def test_that_zero_has_correct_neighbors
+    assert_equal([1, 3, 4], find_neighbors(0, game_array(3)))
+  end
+  def test_that_4_has_correct_neighbors
+    assert_equal(8, find_neighbors(4, game_array(3)).count)
+  end
+  def test_that_1_has_correct_neighbors
+    assert_equal([2, 0, 4, 5, 3], find_neighbors(1, game_array(3)))
+  end
+  def test_that_2_has_correct_neighbors
+    assert_equal([1, 5, 4], find_neighbors(2, game_array(3)))
+  end
+  def test_that_3_has_correct_neighbors
+    assert_equal([4, 6, 7, 0, 1], find_neighbors(3, game_array(3)))
+  end
+  def test_that_5_has_correct_neighbors
+    assert_equal([4, 8, 7, 2, 1], find_neighbors(5, game_array(3)))
+  end   
+  def test_that_6_has_correct_neighbors
+    assert_equal([7, 3, 4], find_neighbors(6, game_array(3)))
+  end
+  def test_that_7_has_correct_neighbors
+    assert_equal([8, 6, 4, 5, 3], find_neighbors(7, game_array(3)))
+  end
+  def test_that_8_has_correct_neighbors
+    assert_equal([7, 5, 4], find_neighbors(8, game_array(3)))
+  end
+       
 end
